1b9926b028c64b3abb045d55d89764b9e7d2b352
Wei <theweifeng@gmail.com>
2019-06-03 14:13:26 -0400

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,24 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(time) {
+  let arr = time.split(":");
+  arr[0] = parseInt(arr[0], 10)
+  let greeting;
+
+  if (arr[0] >= 0 && arr[0] < 12){
+    greeting = "Good Morning";
+  } else if (arr[0] >= 12 && arr[0] < 17) {
+    greeting = "Good Afternoon";
+  } else {
+    greeting = "Good Evening"
+  }
+
+  return greeting
+}
+
+function displayMessage(string) {
+  greeting = document.getElementById('greeting');
+  greeting.innerText = string;
+}
